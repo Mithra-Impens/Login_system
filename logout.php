c63f9f1751a312563cc6417945e15527cd0513dd
Mithra Impens <mithraimpens1312@gmail.com>
2026-05-04 21:55:54 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -3,6 +3,7 @@ session_start();
 
 session_unset();
 session_destroy();
-
+setcookie("remember_username", "", time() - 3600);
+setcookie("user_theme", "", time() - 3600);
 header("Location: login.php");
 exit();
